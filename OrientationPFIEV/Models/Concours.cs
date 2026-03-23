457c50a3148875ca3c54015e351bb2f7620df9c9
QuangTV <quangtv@pfiev.edu.vn>
2026-03-23 10:58:02 +0700

    chore: remove C# source, move artifacts to tmp/, update .gitignore

--- OrientationPFIEV/Models/Concours.cs
+++
@@ -1,9 +0,0 @@
-namespace OrientationPFIEV.Models;
-
-/// <summary>Competition session parameters: year, minimum average, and line count.</summary>
-public class Concours
-{
-    public int Annee { get; set; }
-    public double MoyenneMin { get; set; }
-    public int NumLigne { get; set; }
-}
--- OrientationPFIEV/Models/Matiere.cs
+++
@@ -1,9 +0,0 @@
-namespace OrientationPFIEV.Models;
-
-/// <summary>Subject/exam paper with its weighting coefficient.</summary>
-public class Matiere
-{
-    public int MatiereID { get; set; }
-    public string MatiereNom { get; set; } = "";
-    public double MatiereCoefficient { get; set; }
-}
